32bdcac2353e66fb3531627f4e03e987cb5c8df3
Sanel Zukan <sanel@placeavote.com>
2016-04-08 15:18:23 +0200

    Fixes #3 - adds more secure password storage.
    Now, if password isn't set, pav-conf will query CONVOX_CREDS for getting
    password details, following schema described in #3. Contrary initial
    plan to use ":convox:" or ":env:" keywords, now it will only consult
    environment variables if password key is empty or not set, keeping
    things simple.
    
    This commit also cleans pav-conf.edn from credential details.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.1.0"]
                  [cheshire "5.5.0"]
+                 [environ "1.0.0"]
                  [com.vaadin/vaadin-server "7.6.4"]
                  [com.vaadin/vaadin-client-compiled "7.6.4"]
                  [com.vaadin/vaadin-themes "7.6.4"]
@@ -10,6 +11,7 @@
                  [org.clojure/tools.logging "0.3.0"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]]
+  :plugins [[lein-environ "1.0.0"]]
   :profiles {:jvm-opts ^:replace ["-Xms256m" "-Xmx512m" "-Xss512k" "-XX:MaxMetaspaceSize=150m"]
              :uberjar {:aot :all
                        :uberjar-name "pav-conf.jar"}}
--- src/pav_conf/conf.clj
+++ src/pav_conf/conf.clj
@@ -2,7 +2,20 @@
   "Main configuration reader."
   (:require [clojure.edn :as edn]
             [clojure.tools.logging :as log]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as s]
+            [environ.core :refer [env]]))
+
+(defn- parse-env-creds
+  "Parse environment credentials, stored in CONVOX_CREDS. They are expected to be
+in form 'NAME1:PASS1;NAME2:PASS2;...'. Obviously, take care of having semicolon or
+colons in passwords. Returns a map in form '{:name XXX, :password YYY}'."
+  ([creds]
+     (when creds
+       (map (fn [e]
+              (zipmap [:name :password] (s/split e #"\s*:\s*")))
+            (s/split creds #"\s*;\s*"))))
+  ([] (-> :convox-creds env parse-env-creds)))
 
 (defn- read-conf-raw
   "Read configuration and return map from it."
@@ -14,12 +27,17 @@
   read-conf (memoize read-conf-raw))
 
 (defn- read-creds-raw
-  "Read credentials and convert them to expected convox API format."
-  ([name]
-     (->> (read-conf)
-          :convox-hosts
-          (filter #(= name (:name %)))
-          first)))
+  "Read credentials and convert them to expected convox API format. If password
+is not present, consult environment for it."
+  [name]
+  (let [ret (->> (read-conf)
+                 :convox-hosts
+                 (filter #(= name (:name %)))
+                 first)]
+    (when-not (:password ret)
+      (->> (parse-env-creds)
+           (some #(when (= name (:name %)) %))
+           (merge ret)))))
 
 (def ^{:doc "Memoized version of read-creds-raw."
        :public true}
